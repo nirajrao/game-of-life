da4a045d2c28928b4678d2bb90c5ca57ecb831c2
Qasim Shareh <Qas@Qasims-MacBook-Pro.local>
2016-07-15 19:30:42 -0700

    progess on simul file

--- simul.py
+++ simul.py
@@ -1,6 +1,16 @@
 from cell import Cell
 from board import Board
 
+directions [[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]]
+############   N      NE    E     SE    S     SW     W       NW
+
 class Simulation:
 
-	
+	def __init__(self, boardSize):
+		self.gameBoard = Board(boardSize)
+		####
+
+	def checkNeighbours(self):
+		for rows in self.gameBoard:
+			for cell in rows:
+				if cell.isAlive()
