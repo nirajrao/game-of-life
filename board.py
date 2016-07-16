d9d73e30488b9ca38d451eedeabe113290b28d96
Niraj Rao <nirajrao872@gmail.com>
2016-07-15 18:59:33 -0700

    Fixed Board Error where the same cell was inhabiting the whole row

--- board.py
+++ board.py
@@ -6,19 +6,25 @@ class Board:
 		self.gridSize = gridSize
 		self.grid = []
 		for i in range(0, gridSize):
-			row = [Cell()] * self.gridSize
+			row = []
+			for j in range(0, gridSize):
+				row.append(Cell())
 			self.grid.append(row)
 
 	def print_board(self):
-		for i in range(self.gridSize):
-			print(self.grid[i])
+		for row in self.grid:
+			cell_row = ""
+			for cell in row:
+				cell_row += cell.displaySymbol()
+				cell_row += " "
+			print(cell_row)
 
 	def activateCell(self, row, col):
-		currentCell = self.grid[row][col]
+		currentCell = self.returnCell(row, col)
 		currentCell.activate()
 
 	def deactivateCell(self, row, col):
-		currentCell = self.grid[row][col]
+		currentCell = self.returnCell(row, col)
 		currentCell.deactivate()
 
 	def returnCell(self, row, col):
--- boardTest.py
+++ boardTest.py
@@ -4,27 +4,26 @@ from cell import Cell
 
 class testBoardCreation(unittest.TestCase):
 
-	def testCellActivation(self):
+	def testBoardCreation(self):
 		test_board = Board(8)
-		test_board.activateCell(7, 7)
-		self.assertEqual(test_board.returnCell(7, 7).displaySymbol(), "O")
+		for i in range(test_board.gridSize):
+			for j in range(test_board.gridSize):
+				self.assertEqual(test_board.returnCell(i, j).displaySymbol(), "X")
 
-	def testCellStateChange(self):
+
+	def testRandomCellStateChange(self):
 		test_board = Board(9)
 		test_board.activateCell(3,2)
 		test_board.activateCell(4,2)
 		test_board.activateCell(3,5)
 		test_board.activateCell(7,8)
-		print(test_board.returnCell(7,2).displaySymbol())
 		test_board.activateCell(1,5)
 		test_board.activateCell(7,2)
 		test_board.deactivateCell(3,2)
 		test_board.deactivateCell(7,2)
 		test_board.deactivateCell(4,2)
-		print(test_board.returnCell(7,2).displaySymbol())
-		test_board.print_board()
 		self.assertEqual(test_board.returnCell(7,2).displaySymbol(),"X")
-		# self.assertEqual(test_board.returnCell(7,8).displaySymbol(),"O")
+		self.assertEqual(test_board.returnCell(7,8).displaySymbol(),"O")
 
 
 if __name__ == "__main__":
