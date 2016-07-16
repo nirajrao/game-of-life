0d75d3e07ccfcc2396777df5a012867427f3096e
Niraj Rao <nirajrao872@gmail.com>
2016-07-15 18:14:38 -0700

    Created a cell class to keep track of cell state

--- board.py
+++ board.py
@@ -1,21 +1,21 @@
+from cell import Cell
+
 class Board:
 
 	def __init__(self, gridSize):
 		self.gridSize = gridSize
 		self.grid = []
 		for i in range(0, gridSize):
-			row = [0] * self.gridSize
+			row = [Cell().displaySymbol()] * self.gridSize
 			self.grid.append(row)
 
 	def print_board(self):
 		for i in range(self.gridSize):
 			print(self.grid[i])
 
-			
-
-
 test_board = Board(8)
 test_board.print_board()
+
 
 
 
---
+++ cell.py
@@ -0,0 +1,10 @@
+class Cell:
+
+	def __init__(self):
+		self.isAlive = False
+		self.symbol = "X"
+
+	def displaySymbol(self):
+		return self.symbol
+
+
